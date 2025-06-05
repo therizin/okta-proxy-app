99603709c40a33a61f31dc3b5f44532efabb3900
therizin <therizin@hotmail.com>
2025-06-04 21:49:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,26 @@ app.use('/oauth2', createProxyMiddleware({
   pathRewrite: { '^/oauth2': '/oauth2' },
 }));
 
-app.use('/.well-known', createProxyMiddleware({
-  target: oktaDomain,
-  changeOrigin: true,
-  pathRewrite: { '^/\\.well-known': '/.well-known' },
-}));
+app.get('/.well-known/openid-configuration', (req, res) => {
+  res.json({
+    issuer: "https://okta-proxy-app.onrender.com/oauth2/default",
+    authorization_endpoint: "https://okta-proxy-app.onrender.com/oauth2/default/v1/authorize",
+    token_endpoint: "https://okta-proxy-app.onrender.com/oauth2/default/v1/token",
+    userinfo_endpoint: "https://okta-proxy-app.onrender.com/oauth2/default/v1/userinfo",
+    jwks_uri: "https://okta-proxy-app.onrender.com/oauth2/default/v1/keys",
+    registration_endpoint: "https://okta-proxy-app.onrender.com/oauth2/v1/clients",
+    introspection_endpoint: "https://okta-proxy-app.onrender.com/oauth2/default/v1/introspect",
+    revocation_endpoint: "https://okta-proxy-app.onrender.com/oauth2/default/v1/revoke",
+    response_types_supported: ["code", "id_token", "code id_token", "token id_token", "token"],
+    subject_types_supported: ["public"],
+    id_token_signing_alg_values_supported: ["RS256"],
+    scopes_supported: ["openid", "email", "profile"],
+    token_endpoint_auth_methods_supported: ["client_secret_basic", "client_secret_post"],
+    claims_supported: ["sub", "iss", "name", "email"],
+    code_challenge_methods_supported: ["S256"],
+    grant_types_supported: ["authorization_code", "implicit", "refresh_token"],
+  });
+});
 
 app.use('/idp', createProxyMiddleware({
   target: oktaDomain,
